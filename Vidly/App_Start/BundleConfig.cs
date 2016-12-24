9611873f8668d8805f568ee28dad230b23a71f39
Archer <zaib_qasim@hotmail.com>
2016-12-24 23:48:15 +0500

    Implemented APIs + Client-side display logic

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -4,18 +4,21 @@ using System.Web.Optimization;
 namespace Vidly {
 	public class BundleConfig {
 		public static void RegisterBundles(BundleCollection bundles) {
-			bundles.Add (new ScriptBundle ("~/bundles/jquery").Include (
-						"~/Scripts/jquery-{version}.js"));
+			bundles.Add (new ScriptBundle ("~/bundles/lib").Include (
+						"~/Scripts/jquery-{version}.js",
+						"~/Scripts/bootstrap.js",
+						"~/Scripts/bootbox.js",
+						"~/Scripts/respond.js",
+						"~/Scripts/DataTables/jquery.datatables.js",
+						"~/Scripts/DataTables/datatables.bootstrap.js"));
 			bundles.Add (new ScriptBundle ("~/bundles/jqueryval").Include (
 						"~/Scripts/jquery.validate*"));
 			bundles.Add (new ScriptBundle ("~/bundles/modernizr").Include (
 						"~/Scripts/modernizr-*"));
-			bundles.Add (new ScriptBundle ("~/bundles/bootstrap").Include (
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
 
 			bundles.Add (new StyleBundle ("~/Content/css").Include (
 					  "~/Content/bootstrap-cosmo.css",
+					  "~/Content/DataTables/css/datatables.bootstrap.css",
 					  "~/Content/site.css"));
 		}
 	}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,8 +20,7 @@ namespace Vidly.Controllers {
 		}
 
 		public ActionResult Index() {
-			var movies = _context.Movies.Include(m => m.Genre).ToList ();
-			return View (movies);
+			return View ();
 		}
 
 		[Route ("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
